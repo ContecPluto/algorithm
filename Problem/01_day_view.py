fe43953932903d35b1d6d815be574af34d363e73
Kim <gagle637@gmail.com>
2019-07-29 22:52:33 +0900

    second commit

--- Problem/01_day_view.py
+++ Problem/01_day_view.py
@@ -1,18 +1,26 @@
 import sys
-
 sys.stdin = open('input.txt', 'r')
-N = int(input())
-arr = list(map(int, input().split()))
-count = 0
 
-for i in range(2, len(arr)-2):
-    for j in range(arr[i]+1, 0, -1):
-        for k in range(-2, 2, 1):
-            if k == 0 : continue
-            elif arr[i]-j <= arr[i+k]:
-                break
-        else:
-            count += 1
+for num in range(10):
+    N = int(input())
+    arr = list(map(int, input().split()))
+    count = 0
 
-print(count)
+    # for i in range(2, len(arr)-2):
+    #     for j in range(arr[i], 0, -1):
+    #         for k in range(-2, 2, 1):
+    #             if k == 0 : continue
+    #             elif j <= arr[i+k]:
+    #                 break
+    #         else:
+    #             count += 1
+
+
+    for i in range(2, len(arr)-2):
+        for j in range(arr[i], 0, -1):
+            if j > arr[i-2] and j > arr[i-1] and j > arr[i+1] and j > arr[i+2]:
+                count += 1 
+
+
+    print(f'#{num+1} {count}')
 
