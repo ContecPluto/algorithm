a5be5947fa2903fce6726c0ca064bddc3574abad
Gagle637 <gagle637@gmail.com>
2019-07-31 17:50:49 +0900

    2019-07-31

--- Problem/01_Day/1208_Flatten.py
+++ Problem/01_Day/1208_Flatten.py
@@ -1,24 +1,44 @@
 import sys
 sys.stdin = open('1208.txt', 'r')
 
+
 for num in range(10):
     N = int(input())
     arr = list(map(int, input().split()))
+
 
     # index 사용
     # for i in range(N):
     #     arr[arr.index(max(arr))] -= 1
     #     arr[arr.index(min(arr))] += 1
 
-    for i in range(N):
-        for j in range(len(arr)):
-            if arr[j] == max(arr):
-                arr[j] -= 1
-                break
-        for j in range(len(arr)):
-            if arr[j] == min(arr):
-                arr[j] += 1
-                break
+    # for i in range(N):
+    #     for j in range(len(arr)):
+    #         if arr[j] == max(arr):
+    #             arr[j] -= 1
+    #             break
+    #     for j in range(len(arr)):
+    #         if arr[j] == min(arr):
+    #             arr[j] += 1
+    #             break
 
+    for i in range(N):
+        max_index = arr[0]
+        min_index = arr[0]
+        for j in range(len(arr)):
+            if arr[j] > max_index:
+                max_index = arr[j]
+            if arr[j] < min_index:
+                min_index = arr[j]
+        arr[arr.index(max_index)] -= 1
+        arr[arr.index(min_index)] += 1
+
+    max_index = arr[0]
+    min_index = arr[0]
+    for j in range(len(arr)):
+        if arr[j] > max_index:
+            max_index = arr[j]
+        if arr[j] < min_index:
+            min_index = arr[j]
         
-    print('#{} {}'.format(num+1, max(arr)-min(arr)))
+    print('#{} {}'.format(num+1, max_index-min_index))
