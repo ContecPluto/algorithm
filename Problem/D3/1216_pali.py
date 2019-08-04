0e96e51f9e6e30e17f3f9ed925c12e7d44fa0752
Kim <gagle637@gmail.com>
2019-08-04 23:37:09 +0900

    D3 1216

---
+++ Problem/D3/1216_pali.py
@@ -0,0 +1,30 @@
+import sys
+sys.stdin = open('1216.txt', 'r')
+
+for num in range(10):
+    arr=[]
+    max_arr=0
+    num = input()
+    for i in range(100):
+        numbers = input()
+        arr.append(numbers)
+
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            for k in range(j+1,len(arr[i])+1):
+                cheak = arr[i][j:k]
+                if cheak == cheak[::-1] and max_arr < len(cheak):
+                    max_arr = len(cheak)
+    
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            cheak = ''
+            for k in range(j,len(arr[i])):
+                cheak += arr[k][i]
+                if cheak == cheak[::-1] and max_arr < len(cheak):
+                    max_arr = len(cheak)
+
+    
+    print(max_arr)
+                    
+                    
