f42140e5b50bae23b7f1acd02df111852e18cd04
Kim <gagle637@gmail.com>
2019-08-10 21:28:58 +0900

    2019-08-10

---
+++ Problem/2019-08-07/10815_Number_card.py
@@ -0,0 +1,63 @@
+import sys
+import time
+
+sys.stdin = open('10815.txt','r')
+sang = input()
+sang_card = list(map(int,input().split()))
+answer = int(input())
+answer_card = input()
+
+for i in sang_card:   
+    answer_card = answer_card.replace(str(i), '1')
+answer_card = answer_card.replace('-', '')
+answer_card = list(map(int,answer_card.split()))
+answer_card = [0 if answer_card[i] > 1 else 1 for i in range(answer) ]
+print(answer_card)
+
+
+
+# sang = int(input())
+# sang_card = list(map(int,input().split()))
+# answer = int(input())
+# answer_card = list(map(int,input().split()))
+# # print(len(sang_card))
+# sang_card.sort()
+# result = [0]*answer
+
+# for i in range(answer):
+#     # print(answer_card)
+#     pop_card = answer_card.pop()
+#     for j in range(len(sang_card)):
+#         if sang_card[j] == pop_card:
+#             answer_card.insert(i, 1)
+#             del sang_card[j]
+#             break
+#     else:
+#         answer_card.insert(i, 0)        
+# print(answer_card)
+
+
+# for i in range(len(answer_card)):
+#     s,l = 0 , sang
+#     c = int((s + l)/2)
+#     if sang_card[s] == answer_card[i]:
+#         result[s] += 1
+#     elif sang_card[l-1] == answer_card[i]:
+#         result[answer-1] += 1
+#     else:        
+#         while s != c:            
+#             if answer_card[i] == sorted(sang_card)[c]:
+#                 # print('찾음', i)
+#                 result[i] += 1
+#                 break
+#             elif answer_card[i] > sorted(sang_card)[c]:
+#                 # print('큰쪽', i)
+#                 # print(s,c,l)
+#                 s = c
+#                 c = int((s+l)/2)
+#             else:
+#                 # print('작은쪽', i)
+#                 l = c
+#                 c = int((s+l)/2)
+
+# print(result)
--- Problem/2019-08-07/2578_binggo.py
+++ Problem/2019-08-07/2578_binggo.py
@@ -1,39 +1,51 @@
 import sys
 sys.stdin = open('2578.txt', 'r')
-binggo = [list(map(int, input().split())) for _ in range(5)]
-mc = [list(map(int, input().split())) for _ in range(5)]
-count = [[0]*5 for _ in range(5)]
+
+binggo = []
+mc = []
+count = [0]*25
 x_bin,y_bin, cross, cross2 =0,0,0,0
 n_count = 0
 b_count = 0
 
-for i in range(len(binggo)):
-    for j in range(len(binggo[0])):
-        n_count += 1
-        # print(mc[i][j])
-        for k in range(len(binggo)):
-            if mc[i][j] in binggo[k]:
-                # print(mc[i][j], binggo[k].index(mc[i][j]))
-                count[k][binggo[k].index(mc[i][j])] += 1                
-                break 
 
-        for x in range(5):
-            cross += count[x][x]
-            cross2 += count[x][4-x]
-            for y in range(5):
-                x_bin += count[x][y]
-                y_bin += count[y][x]
-            if x_bin == 5:
-                b_count += 1
-            if y_bin == 5:
-                b_count += 1
-            x_bin, y_bin = 0, 0
-        if cross == 5:
-            b_count +=1
-        if cross2 == 5:
-            b_count +=1
-        cross,cross2 = 0, 0    
+# for i in range(25):
+#     count[i] += 1*i
+# print(count[4:24:4])
+
+for _ in range(5):
+    binggo += list(map(int, input().split()))
+for _ in range(5):
+    mc += list(map(int, input().split()))
 
 
-print(b_count)
+# print('시작')
+for idx,number in enumerate(mc):
+    n_count += 1
+    count[binggo.index(number)] += 1
+    # print(count)
+    for i in range(5):
+        # print(i)
+        if sum(count[i*5:i*5+5]) == 5:
+            # print('x축', count[i*5:i*5+5])
+            b_count += 1
+        if sum(count[i::5]) == 5:
+            # print('y축')
+            b_count += 1
+    if sum(count[::6]) == 5:
+        # print('대각선1')
+        b_count += 1
+    if sum(count[4:len(binggo)-1:4]) == 5:
+        # print('대각선2')
+        b_count += 1
+    if b_count >= 3:        
+        print(n_count)
+        break
+    # print('끝')
+    b_count = 0
+    
+
+
+    
+
 
