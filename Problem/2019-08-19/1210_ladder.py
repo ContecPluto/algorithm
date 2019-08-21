d6790853e9b1d8dc0dc752de3e26b195581a9a60
Kim <gagle637@gmail.com>
2019-08-21 23:02:57 +0900

    BFS

--- Problem/2019-08-19/1210_ladder.py
+++ Problem/2019-08-19/1210_ladder.py
@@ -1,40 +1,35 @@
-
-
-
 import sys
 sys.stdin = open('1210.txt', 'r')
 
-for num in range(1):
-    n = int(input())
+for num in range(10):
+    n = input()
     ladder = [list(map(int, input().split())) for _ in range(100)]
-    ladder_index = [[] for _ in range(100)]
+    dx = [0, 0, -1]
+    dy = [-1, 1, 0]
     for x in range(100):
-        for y in range(100):
-            if ladder[x][y]:
-                ladder_index[x].append(y)
-    # for x in range(100):
-    #     print(x, '-->', ladder_index[x])
+        if ladder[99][x] == 2:
+            y = x
+            break    
+    x = 99
+    # print(y)
 
-    visit = [[False] * 100] * 100
-    for x in range(1):
-        for y in ladder_index[x]:
-            print(x,y)
-            visit[x][y] = True
-    print(visit[0])
-
-
-
-
-
-# def DFS(v):
-#     visit[0][v] = True;
-#     print(v, end=' ')
-#     for w in ladder_index[v]:
-#         if not visit[w]:
-#             DFS(w)
-#
-# DFS(0)
-
-
-
+    # y = ladder[99].index(2)
+    
+    while x != 0:
+        # print(x,y)
+        ladder[x][y] = 0
+        for i in range(3):
+            tx = x + dx[i]
+            ty = y + dy[i]
+            if 0 <= ty <= 99:
+                if ladder[tx][ty] != 0:
+                    # print(tx,ty)
+                    # print('잡힘?')
+                    x = tx
+                    y = ty
+                    break
+        if x == 0:
+            print('#{} {}'.format(n, y))
+            break
+    
 
