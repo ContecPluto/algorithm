a8808e2279992c4908e346120cfaee3c94ed551e
Gagle637 <gagle637@gmail.com>
2019-08-31 18:40:05 +0900

    mac algorithm problem stack2

--- Problem/2019-08-27/4880_tournament.py
+++ Problem/2019-08-27/4880_tournament.py
@@ -1,55 +1,33 @@
-def tournament(v):
-    # print('v=', v)
-    winner = []
-    if len(v) > 1:
-        for i in range(0, len(v), 2):
-            # print(i)
-            if v[i+1:i+2]:
-                if v[i][0] == 1:
-                    if v[i+1][0] == 1 or v[i+1][0] == 3:
-                        winner.append(v[i])
-                    else:
-                        winner.append(v[i+1])
-                if v[i][0] == 2:
-                    if v[i+1][0] == 2 or v[i+1][0] == 1:
-                        winner.append(v[i])
-                    else:
-                        winner.append(v[i + 1])
-                if v[i][0] == 3:
-                    if v[i+1][0] == 3 or v[i+1][0] == 2:
-                        winner.append(v[i])
-                    else:
-                        winner.append(v[i + 1])
-                # print('위너1', winner)
-            else:
-                # print('위너2', v[i])
-                # print(winner)
-                winner.append(v[i])
-        # print('인풋', winner)
-        tournament(winner)
-    # print(v)
-    else:
-        check.append(v[0])
+def tournament(lo, hi):
+    if lo == hi:
+        return lo
 
-def devison(list):
+    mid = int((lo + hi)/2)
+
+    l = tournament(lo, mid)
+    r = tournament(mid + 1, hi)
+
+    if student[l] == 1:
+        if student[r] == 1 or student[r] == 3:
+            return l
+        else:
+            return r
+    if student[l] == 2:
+        if student[r] == 2 or student[r] == 1:
+            return l
+        else:
+            return r
+    if student[l] == 3:
+        if student[r] == 3 or student[r] == 2:
+            return l
+        else:
+            return r
+
 
 import sys; sys.stdin = open('4880.txt', 'r')
 
 T = int(input())
 for tc in range(1, T+1):
-    check = []
     n = int(input())
-    student = list(map(list, input().split()))
-    for i in range(len(student)):
-        student[i][0] = int(student[i][0])
-        student[i].append(i+1)
-    # print(student)
-    #
-    # if len(st)
-    # print(student[:(len(student)+1)//2])
-    # print(student[((len(student)+1)//2)::])
-    left = tournament(student[:(len(student)//2+1)])
-    right = tournament(student[(len(student)//2)+1::])
-    # print(check)
-    tournament(check)
-    print('#{} {}'.format(tc, check[len(check)-1][1]))
+    student = list(map(int, input().split()))
+    print('#{} {}'.format(tc, tournament(0, len(student) - 1) + 1))
--- Problem/2019-08-27/4881_array_min.py
+++ Problem/2019-08-27/4881_array_min.py
@@ -1,43 +1,32 @@
 import sys; sys.stdin = open('4881.txt', 'r')
 
-def perm(k, n, visit):   #visit: 지금까지 선택한 요소들의 집합
+def perm(k, s):
     global result
-    if k == n:
-        check = []
-        for i in range(N):
-            check.append(arr[order[i]])
-        result.append(check)
+    if k == N:
+        if result > s:
+            result = s
+        return
+    elif result <= s:
+        return
     else:
         #아직 선택되지 않은 요소들을 찾는다.
-        for i in range(n):
-            if visit & (1 << i): continue
-            order[k] = i
-            perm(k + 1, n, visit | (1 << i))
+        for i in range(N):
+            if order[i] == 0:
+                order[i] = 1
+                perm(k + 1, s + arr[k][i])
+                order[i] = 0
+        return
+
 
 
 T = int(input())
 for tc in range(1, T+1):
-    #부분 합 -----------
-    result = []
+    #순열 합 -----------
+    result = 1000
     N = int(input())
-    arr = list(range(N))
-    order = [0] * N
-    perm(0, N, 0)
-    #부분 합 -------------- 결과값은 result를 이용해 사용함
+    order = [0 for _ in range(N)]
+    arr = [list(map(int, input().split())) for i in range(N)]
+    perm(0, 0)
+    #순열 합 -------------- 결과값은 result를 이용해 사용함
 
-    total = 0
-    min_arr = 100000
-
-    # print(result)
-    arr2 = [list(map(int, input().split())) for i in range(N)]
-    # print(arr2)
-    for x in range(len(arr2)):
-        for i in result:
-            total = 0
-            for j in i:
-                # print(x,i[j])
-                # print(arr2[x][i[j]])
-                total += arr2[j][i[j]]
-            # print(total)
-            min_arr = min(total, min_arr)
-    print('#{} {}'.format(tc, min_arr))
+    print('#{} {}'.format(tc, result))
