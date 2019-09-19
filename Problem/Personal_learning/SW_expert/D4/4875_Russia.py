5fc7ca81899072fd17c81e46809622f929d7d1c3
Gagle637 <gagle637@gmail.com>
2019-09-19 22:56:13 +0900

    fail

--- Problem/Personal_learning/SW_expert/D4/4875_Russia.py
+++ Problem/Personal_learning/SW_expert/D4/4875_Russia.py
@@ -2,30 +2,26 @@ import sys; sys.stdin = open('4875.txt','r')
 
 T = int(input())
 color = ['W', 'B', 'R']
-for tc in range(T):
+for tc in range(1, T+1):
     index = 0
     N, M = list(map(int, input().split()))
-    flag = [input() for _ in range(N)]
-    count = 0
-    for i in range(N):
-        if index != 2:
-            if flag.count(color[index]) <= flag.count(color[index+1]):
-                index += 1
-        if i == 0:
-            index = 0
-        elif i == (N-1):
-            index = 2
+    flag = [list(input()) for _ in range(N)]
+    check = []
+    flag_color = ['B' for _ in range(N)]
+    flag_color[0] = 'W'
+    flag_color[-1] = 'R'
+    print(flag_color)
+    for x in range(1, N-1):
+        midle = []
+        for i in range(2):
+            for y in range(N):
+                # print(N, flag[y].count(flag_color[y]))
+                midle.append(M-flag[y].count(flag_color[y]))
+            if flag_color[x - 1] != flag_color[x]:
+                flag_color[x] = flag_color[x-1]
+            if flag_color[N - x] != flag_color[N - x -1]:
 
-        for x in range(M):
-            if flag[i][x] != color[index]:
-                count += 1
-    print(count)
-
-
-
-
-
-
-
-
+        print(midle)
+        check.append(sum(midle))
+    print(check)
 
