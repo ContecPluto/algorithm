b24e574b3056652bc9d4d82dc4cfa89b3745841d
Gagle637 <gagle637@gmail.com>
2019-09-20 18:02:26 +0900

    2019-09-20

--- Problem/Personal_learning/SW_expert/D4/4875_Russia.py
+++ Problem/Personal_learning/SW_expert/D4/4875_Russia.py
@@ -10,18 +10,10 @@ for tc in range(1, T+1):
     flag_color = ['B' for _ in range(N)]
     flag_color[0] = 'W'
     flag_color[-1] = 'R'
-    print(flag_color)
-    for x in range(1, N-1):
-        midle = []
-        for i in range(2):
-            for y in range(N):
-                # print(N, flag[y].count(flag_color[y]))
-                midle.append(M-flag[y].count(flag_color[y]))
-            if flag_color[x - 1] != flag_color[x]:
-                flag_color[x] = flag_color[x-1]
-            if flag_color[N - x] != flag_color[N - x -1]:
 
-        print(midle)
-        check.append(sum(midle))
-    print(check)
+    for i in range(N):
+        for j in range(M):
+            if flag[i][j] != flag_color[i]:
+                flag[i][j] = flag_color[i]
+
 
