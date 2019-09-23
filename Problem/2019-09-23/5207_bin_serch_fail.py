790184cac52b69f0a7c13c73f2254d0f756447e7
Kim <gagle637@gmail.com>
2019-09-23 22:36:10 +0900

    5207_fail

--- Problem/2019-09-23/5207_bin_serch_fail.py
+++ Problem/2019-09-23/5207_bin_serch_fail.py
@@ -3,36 +3,28 @@ import sys; sys.stdin = open('5207.txt', 'r')
 def BinarySearch(lo, hi, search):
     global cnt, check
     mid = (lo + hi) // 2
-    if [lo, hi, mid] == check:
+    if lo > hi:
         return
-    check = [lo, hi, mid]
-    print(search, A[mid])
-    print(lo, hi, mid)
-    print()
+
+    # print(mid, A[mid], search)
     if A[mid] == search:
         cnt += 1
         return
-
-    # if mid == hi or lo == mid:
-    #     return
     elif A[mid] > search:
+        check.append(2)
         BinarySearch(lo, mid - 1, search)
     elif A[mid] < search:
+        check.append(1)
         BinarySearch(mid + 1, hi, search)
 
-
-
 T = int(input())
-for tc in range(1, T+1):
+for tc in range(1, T + 1):
     N, M = list(map(int, input().split()))
     A = sorted(list(map(int, input().split())))
     B = list(map(int, input().split()))
     cnt = 0
-    print(A)
-
     for i in B:
         check = []
-        BinarySearch(0, N-1, i)
-
+        BinarySearch(0, len(A)-1, i)
     print('#{} {}'.format(tc, cnt))
 
