57b80351dc7085fedbce7de1f780c03c0f9ad913
Kim <gagle637@gmail.com>
2019-10-07 02:05:21 +0900

    2019-10-07 1808 save

--- Problem/2019-10-04/1808_Calculator.py
+++ Problem/2019-10-04/1808_Calculator.py
@@ -1,41 +1,64 @@
 import sys; sys.stdin = open('1808.txt', 'r')
 
-def comb(k, num):
+def check(k, CX, N):
     global flag
-    if k == len(num):
-        print(choose)
+    if k == N:
+        print('찾음')
         flag = 1
         return
     for i in arr:
-        if int(num[k]) == i:
-            choose.append(i)
-            comb(k + 1, X)
-            choose.pop()
+        if i == CX[k]:
+            chose.append(i)
+            check(k + 1, CX, N)
 
-def comb2(k, num, div):
+def comb(div, num):
     global flag
-    if num == 0:
-        flag = 1
-        return
     if flag == 1:
         return
-    comb(0, str(num))
+    if div > int(X):
+        return
+    # print(div, num, num%div)
+    # print(div)
+    if div == num:
+        flag = 1
+        print('찾음')
+        return
+
+    if num%div >= 100:
+        return
+    if num%div == 0:
+        CX = list(map(int, str(num//div)))
+        check(0, CX, len(CX))
     for i in arr:
-        comb2(k, int(num)//div, (div*10) + i)
-        comb2(k, int(num)//div, (div*i))
+        comb((div*10) + i, num)
+        div_num = div
+        for j in range(1, 3):
+            # div_num = (div_num * 10) + i
+            # comb(div_num, num)
+            # comb((div * 10) + i, num)
+            comb(((div * 10) + i) ** j, num)
+            comb(((div * 10) + i) * (div ** j), num)
+            # comb(((div * 10) + i) ** (j - 1), num)
+            # comb(((div*10) + i) * ((div*10) + i), num)
+        # comb(((div*10) + i) * div, num)
+        if i == 0 or i == 1: continue
+        comb(div * i, num)
+
 
 T = int(input())
 for tc in range(T):
+    print('tc:',tc+1)
     flag = 0
     arr = list(map(int, input().split()))
     X = input()
+    first_check = list(map(int, X))
+    length = len(first_check)
+    chose = []
     arr = [i for i in range(len(arr)) if arr[i] == 1]
-    choose = []
-    num = int(X)
-    comb(0, X)
-    if flag == 0:
-        for div in arr:
-            if div == 0: continue
-            comb2(0, X, div)
-
-
+    check(0, first_check, length)
+    print(arr, X)
+    for i in arr:
+        if i == 0: continue
+        if flag == 1: continue
+        comb(i, int(X))
+    print()
