526621c36aa98d0b2add9b0e84046b187ec2e9da
Gagle637 <gagle637@gmail.com>
2020-03-08 00:04:10 +0900

    SW expert 3459 complete

--- Problem/Personal_learning/SW_expert/D4/3459_winner.py
+++ Problem/Personal_learning/SW_expert/D4/3459_winner.py
@@ -6,9 +6,12 @@ for tc in range(1, T + 1):
     N = int(input())
     winner = True
     num = 1
-    while N > num:
-        num *= 2
+    while N > 0:
+        N -= num
+        if winner:
+            num *= 4
         winner = not winner
+
     win = 'Alice' if winner else 'Bob'
     res.append(f'#{tc} {win}')
 print('\n'.join(res))
