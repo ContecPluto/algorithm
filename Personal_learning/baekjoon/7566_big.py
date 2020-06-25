3769603de4c9a533d9b86d2f88c710c1ae7c7929
Gagle637 <gagle637@gmail.com>
2020-06-25 23:43:39 +0900

    baekjoon 7566 big

---
+++ Personal_learning/baekjoon/7566_big.py
@@ -0,0 +1,14 @@
+N = int(input())
+humans = []
+for val in range(N):
+    humans.append(list(map(int, input().split())))
+
+result = ''
+for human in humans:
+    rank = 1
+    for target in humans:
+        if human[0] < target[0] and human[1] < target[1]:
+            rank += 1
+    result += str(rank) + ' '
+
+print(result)
