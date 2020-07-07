29039eb7566240d202c4ec10c42764e644e09db6
Gagle637 <gagle637@gmail.com>
2020-07-07 23:27:18 +0900

    Update baekjoon 14888 operator

---
+++ Personal_learning/baekjoon/14888_operator.py
@@ -0,0 +1,22 @@
+def calculation(n, result, plus, minus, multi, div):
+    if n == N:
+        max_val = max(max_val, result)
+        min_val = min(min_val, result)
+        return
+    if plus != 0:
+        calculation(n + 1, result + numbers[n], plus - 1, minus, multi, div)
+    if minus != 0:
+        calculation(n + 1, result - numbers[n], plus, minus - 1, multi, div)
+    if multi != 0:
+        calculation(n + 1, result * numbers[n], plus, minus, multi - 1, div)
+    if div != 0:
+        calculation(n + 1, result // numbers[n], plus, minus, multi, div - 1)
+    return
+
+N = int(input())
+numbers = list(map(int, input().split(' ')))
+max_val = 0
+min_val = 0xffffffff
+opers = list(map(int, input().split(' ')))
+# print(opers[0] - 1)
+calculation(1, numbers[0], opers[0], opers[1], opers[2], opers[3])
