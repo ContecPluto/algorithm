5a126a8422d863b41d57bc7ce25e76eebd188d76
Gagle637 <gagle637@gmail.com>
2020-07-08 03:05:20 +0900

    Update baekjoon 14888 operator

--- Personal_learning/baekjoon/14888_operator.py
+++ Personal_learning/baekjoon/14888_operator.py
@@ -1,24 +1,25 @@
 def calculation(n, result, plus, minus, multi, div):
+    global max_val, min_val
     if n == N:
         max_val = max(max_val, result)
         min_val = min(min_val, result)
         return
-    if plus != 0:
+    if plus:
         calculation(n + 1, result + numbers[n], plus - 1, minus, multi, div)
-    if minus != 0:
+    if minus:
         calculation(n + 1, result - numbers[n], plus, minus - 1, multi, div)
-    if multi != 0:
+    if multi:
         calculation(n + 1, result * numbers[n], plus, minus, multi - 1, div)
-    if div != 0:
-        calculation(n + 1, result // numbers[n], plus, minus, multi, div - 1)
+    if div:
+        calculation(n + 1, int(result / numbers[n]), plus, minus, multi, div - 1)
     return
+
 
 N = int(input())
 numbers = list(map(int, input().split(' ')))
-max_val = 0
+max_val = -0xffffffff
 min_val = 0xffffffff
 opers = list(map(int, input().split(' ')))
-# print(opers[0] - 1)
 calculation(1, numbers[0], opers[0], opers[1], opers[2], opers[3])
 print(max_val)
 print(min_val)
