16ae153b2fd591c756d8ed2ea68df5846356b401
Gagle637 <gagle637@gmail.com>
2020-07-13 18:36:42 +0900

    Update baekjoon 1149_RGB

--- Personal_learning/baekjoon/1149_RGB.py
+++ Personal_learning/baekjoon/1149_RGB.py
@@ -1,16 +1,29 @@
-dx = [-1, +1]
+# 풀이 1
+# import sys
 
-N = int(input())
-arr = [0xfffffffff] * N
-origin = set(range(N))
-matrix = [[0] * N for _ in range(N)]
-for x in range(N):
-    matrix[x] = list(map(int, input().split(" ")))
-    if x == 0: continue
-    for i in range(3):
-        num = 0xffffffffff
-        for j in range(3):
-            if i == j: continue
-            num = min(num, matrix[x][i] + matrix[x-1][j])
-        matrix[x][i] = num
+# N = int(input())
+# matrix = [[0] * N for _ in range(N)]
+# matrix[0] = list(map(int, sys.stdin.readline().split(" ")))
+# for x in range(1, N):
+#     matrix[x] = list(map(int, input().split(" ")))
+#     if x == 0: continue
+#     for i in range(3):
+#         num = 0xffffffffff
+#         for j in range(3):
+#             if i == j: continue
+#             num = min(num, matrix[x][i] + matrix[x-1][j])
+#         matrix[x][i] = num
+# print(min(matrix[-1]))
+
+
+# 풀이 2 같은 방식(간단 코딩)
+import sys
+
+N = int(sys.stdin.readline())
+matrix = [list(map(int, sys.stdin.readline().split(" ")))]
+for x in range(1, N):
+    matrix.append(list(map(int, sys.stdin.readline().split(" "))))
+    matrix[x][0] += min(matrix[x-1][1], matrix[x-1][2])
+    matrix[x][1] += min(matrix[x-1][0], matrix[x-1][2])
+    matrix[x][2] += min(matrix[x-1][0], matrix[x-1][1])
 print(min(matrix[-1]))
