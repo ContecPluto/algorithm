19a78ac19d60ae79ef5f22c789722876b2d11622
Gagle637 <gagle637@gmail.com>
2020-07-23 01:05:13 +0900

    Update baekjoon 12862 back

--- Personal_learning/baekjoon/12865_back.py
+++ Personal_learning/baekjoon/12865_back.py
@@ -1,13 +1,9 @@
 N, K = map(int, input().split(" "))
 stuff = [[0,0]] + [list(map(int, input().split(" "))) for _ in range(N)]
-dp = [[0] * (K + 1) for _ in range(N)]
-ran = set()
-# if stuff[0][0] < K:
-#     dp[stuff[0][0]] = stuff[0][1]
-#     ran.add(stuff[0][0])
+dp = [0] * (K + 1)
+
 for i in range(1, N + 1):
-    print(stuff[i][0])
-    for j in range(1, stuff[i][0] + 1):
-        dp[i][j] = max(dp[i-1][j], dp[i-1][j-stuff[i][0]+stuff[i][1]])
-    print(dp)
-print(max(dp))
+    w, v = stuff[i]
+    for j in range(K - w, -1, -1):
+        dp[w + j] = max(dp[w + j], dp[j] + v)
+print(dp[K])
