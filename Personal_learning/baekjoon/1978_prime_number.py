5846a5f001092a98b81d6b2b2f49414a0f62e9c0
Gagle637 <gagle637@gmail.com>
2020-07-28 02:08:17 +0900

    Update baekjoon 1978 prime number

--- Personal_learning/baekjoon/1978_prime_number.py
+++ Personal_learning/baekjoon/1978_prime_number.py
@@ -1,9 +1,12 @@
+from sys import stdin
+input = stdin.readline
+
 N = int(input())
 numbers = list(map(int, input().split()))
 result = 0
 for number in numbers:
-    if number != 1:
-        for i in range(2, number):
+    if number != 1 and number % 2 != 0 or number == 2:
+        for i in range(3, number//2, 2):
             if number % i == 0:
                 break
         else:
