6e643fe2201ea3d5005259de0c481cf29499fb92
Gagle637 <gagle637@gmail.com>
2020-07-28 17:47:08 +0900

    Update baekjoon 2581 prime number

--- Personal_learning/baekjoon/2581_pirme_number.py
+++ Personal_learning/baekjoon/2581_pirme_number.py
@@ -1,11 +1,15 @@
+from math import sqrt
+
 N = int(input())
 M = int(input())
+if N == 1:
+    N += 1
 
 result = []
 for number in range(N, M + 1):
-    if number != 1 and number % 2 != 0 or number == 2:
-        for i in range(3, number//2, 2):
-            if number % i == 0:
+    if number % 2 or number == 2:
+        for i in range(3, int(sqrt(number)) + 1, 2):
+            if not number % i:
                 break
         else:
             result.append(number)
