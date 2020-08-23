b1b3897600b28f9b6fc2f0b70229ac78a5d302a3
Gagle637 <gagle637@gmail.com>
2020-08-23 23:38:16 +0900

    Add baekjoon 11049 procession

---
+++ Personal_learning/baekjoon/11049_procession.py
@@ -0,0 +1,16 @@
+N = int(input())
+dp = [[0] * N for _ in range(N)]
+procession = [list(map(int, input().split())) for _ in range(N)]
+
+for i in range(N-1):
+    x, y = procession[i]
+    z = procession[i+1][1]
+    dp[i][i+1] = x * y * z
+
+arr = list(range(N))
+for i in range(1, N):
+    for j in range(N-i):
+        dp[j][i+j] = 0xfffff
+        for k in range(j, j+i):
+            dp[j][i+j] = min(dp[j][i+j], (dp[j][k] + dp[k+1][i+j] + procession[j][0] * procession[k][1] * procession[i+j][1]))
+print(dp[0][N-1])
