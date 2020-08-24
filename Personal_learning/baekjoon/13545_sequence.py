9eed40ba249c0a3e4076256d1edc51d23b7c1f69
Gagle637 <gagle637@gmail.com>
2020-08-25 00:42:43 +0900

    Add baekjoon 13545 sequence | fail

--- Personal_learning/baekjoon/13545_sequence.py
+++ Personal_learning/baekjoon/13545_sequence.py
@@ -1,45 +1,14 @@
 N = int(input())
 sequence = [0] + list(map(int, input().split()))
 M = int(input())
-data = {0: {
-    1: 0,
-    -1: 0
-}}
-# print(data[0][1])
-for i in range(1, N + 1):
-    # print(x)
-    # print(data[i-1][1])
-    data[i] = {1:0, -1:0}
-    data[i][1] = data[i-1][1]
-    data[i][-1] = data[i-1][-1]
-    data[i][sequence[i]] += 1
-    # sequence[i] += sequence[i-1]
-# print(sequence)p
-print(data)
+
+for i in range(1, N+1):
+    sequence[i] += sequence[i-1]
+
 result = ""
 for m in range(M):
     i, j = map(int, input().split())
-    # i -= 1
-    # j -= 1
-    # j -= 1
-    # val = sequence[j] - sequence[i]
-    # print()
-    # val = len(ls) - sum(ls)
-    # print(sum(sequence[i:j]))
-    # val = 0
-    # visit = [False] * N
-    # for x in range(i, j):
-    #     if visit[x]: continue
-    #     for y in range(j):
-    #         if x == y or visit[y]: continue
-    #         if sequence[x] + sequence[y] == 0:
-    #             visit[x], visit[y] = True, True
-    #             val += 2
-    #             break
-    # print(j - i + 1)
-    # val = j - i 
-    # val = sequence[j] - sequence[i]
-    # print(j, i, sequence[j], sequence[i])
-    # val = j - i + 1 - sequence[j] + sequence[i-1]
-    # result += f"{val}\n"
+    i -= 1
+    val = j - i - abs(sequence[j] + sequence[i])
+    result += f"{val}\n"
 print(result, end="")
