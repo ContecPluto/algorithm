9537cb93f159752ef2afd0bec78fe5d964ddbdb7
Gagle637 <gagle637@gmail.com>
2020-08-26 00:58:19 +0900

    Update baekjoon 2636 chess solving

--- Personal_learning/baekjoon/2636_chess.py
+++ Personal_learning/baekjoon/2636_chess.py
@@ -2,39 +2,49 @@ from sys import stdin
 from collections import deque
 input = stdin.readline
 
-def air(ai, aj):
-    air_index = deque([ai, aj])
+def hole_check(ax, ay):
+    for adx, ady in D:
+        tax = ax + adx
+        tay = ay + ady
+        if 0 <= tax < N and 0 <= tay < M and chess[tax][tay] == -1:
+            air_check(tax, tay)
+            break
+
+def air_check(ai, aj):
+    air_index = deque([[ai, aj]])
     while air_index:
         ax, ay = air_index.popleft()
         for adx, ady in D:
             tax = ax + adx
             tay = ay + ady
-            if 0 <= tx < M and 0 <= ty < N and chess[tx][ty] == 0:
-                chess[x][y] = -1
+            if 0 <= tax < N and 0 <= tay < M and chess[tax][tay] == 0:
+                chess[tax][tay] = -1
+                air_index.append([tax, tay])
 
 D = ((0, -1), (0, 1), (1, 0), (-1, 0))
 N, M = map(int, input().split())
 chess = [list(map(int, input().split())) for _ in range(N)]
-index = [[i, j]  for i in range(N) for j in range(M) if chess[i][j] == 1]
+index = [[i, j] for i in range(N) for j in range(M) if chess[i][j] == 1]
 index = deque(index)
-cnt = 0
-air(0, 0)
-hole_idx = [[i, j]  for i in range(N) for j in range(M) if chess[i][j] == 0]
-
-
-
+cnt = len(index)
+result = 0
+air_check(0, 0)
 while index:
     air = []
     in_chess = []
+    is_hole = []
     while index:
         x, y = index.popleft()
         is_air = False
         for dx, dy in D:
             tx = x + dx
             ty = y + dy
-            if 0 <= tx < M and 0 <= ty < N and chess[tx][ty] == -1:
-                is_air = True
-                break
+            if 0 <= tx < N and 0 <= ty < M:
+                if chess[tx][ty] == -1:
+                    is_air = True
+                elif chess[tx][ty] == 0:
+                    is_hole.append([tx, ty])
+
         if is_air:
             air.append([x, y])
         else:
@@ -42,10 +52,17 @@ while index:
 
     for i, j in air:
         chess[i][j] = 0
+        is_hole.append([i, j])
 
+    for i, j in is_hole:
+        air_check(i, j)
     index = deque(in_chess)
+
     if len(in_chess):
         cnt = len(in_chess)
-    print(*chess, sep="\n")
-    print()
+    result += 1
+
+    # print(*chess, sep="\n", end="\n\n")
+    
+print(result)
 print(cnt)
