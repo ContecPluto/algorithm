575912f428e8f6f8c93487132f71edc10418a87b
Gagle637 <gagle637@gmail.com>
2020-09-07 00:43:07 +0900

    Update programmers travel route | pass

--- Personal_learning/programmers/DFS:BFS/travel_route.py
+++ Personal_learning/programmers/DFS:BFS/travel_route.py
@@ -1,21 +1,31 @@
-from heapq import heappop, heappush
+answer = []
+
+def DFS(s, tickets, visit, route):
+    global answer
+    if len(route) == len(tickets)+1:
+        answer = route
+        return
+
+    if answer:
+        return
+
+    for idx in range(len(tickets)):
+        a, b = tickets[idx]
+        if a == s:
+            if visit[idx] == False:
+                visit[idx] = True
+                DFS(b, tickets, visit, route+[b])
+                visit[idx] = False
 
 def solution(tickets):
-    air_route = {}
-    tickets = sorted(tickets, key=lambda x: x[0])
-    Q = []
+    global answer
+    tickets = sorted(tickets, key=lambda x: (x[0], x[1]))
     answer = []
     N = len(tickets) + 1
-
-    for a, b in tickets:
-        heappush(Q, [a, [a]])
-        if air_route.get(a) == None:
-            air_route[a] = [b]
-        else:
-            air_route[a].append(b)
-            air_route[a] = sorted(air_route[a])
-    print(air_route)
-
+    visit = [False] * len(tickets)
+    DFS("ICN", tickets, visit, ["ICN"])
+    return answer
 
 # print(solution([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]))
 print(solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL", "SFO"]]))
+# print(solution([["ICN", "A"], ["A", "C"], ["A", "D"], ["D", "B"], ["B", "A"]]))
