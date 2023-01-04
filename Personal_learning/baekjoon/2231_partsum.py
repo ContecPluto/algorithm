d08773d39b65e95cddb0684c51446e45c35aa4d4
Gagle637 <gagle637@gmail.com>
2023-01-05 00:34:24 +0900

    Update 백준 2231 부분합 불합격 문제 효율 개선

--- Personal_learning/baekjoon/2231_partsum.py
+++ Personal_learning/baekjoon/2231_partsum.py
@@ -1,18 +1,27 @@
+# https://www.acmicpc.net/problem/2231
 import sys; sys.stdin = open('2231.txt')
 
-num = input()
-target = int(num)
-N = len(num)
+# 브루트포스는 모든 경우의수를 대조해보는 알고리즘으로
+# 약간의 조건을 추가하여 시간 및 메모리를 비약적으로 감소시킬 수 있다.
+
+num = int(input())
 result = 0
 
-for i in range(int(num)):
-    res = i
-    
-    for j in str(i):
-        res += int(j)
+# 부분합과 타겟의 숫자차이는 크지 않음.
+# 문제에서 요구하는 조건에 따르면 9가 알맞은것으로 보임.
+# 변수는 백준의 정답리스트중에서 가져온것이다.
+x = num - len(str(num)) * 9
 
-    if (target == res):
+# 부분합의 숫자는 원본의 절반 이상을 절대로 넘을수 없다.
+start = max(x, num // 2)
+for i in range(start, num):
+    res = i + sum(map(int, str(i)))
+
+    if num == res:
         result = i
         break
+
+
+    
 
 print(result)
